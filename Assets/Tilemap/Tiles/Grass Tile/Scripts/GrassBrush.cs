b0b6a39085561ca10cdeb1def50b98eefa5c3347
zengyunhan <zengyunhan@thinkplay.co>
2019-05-30 16:43:24 +0800

    add grass tile

---
+++ Assets/Tilemap/Tiles/Grass Tile/Scripts/GrassBrush.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+
+namespace UnityEngine.Tilemaps
+{
+    [Serializable]
+    [CustomGridBrush(true, false, false, "Grass Brush")]
+    [CreateAssetMenu(fileName = "New Grass Brush", menuName = "Brushes/Grass Brush")]
+#if UNITY_EDITOR
+    public class GrassBrush : GridBrush {
+#else
+    public class GrassBrush : GridBrushBase {
+#endif
+        [SerializeField]
+        public GrassTile Tile;
+
+        GridInformation GetOrCreateInfo(Tilemap tilemap)
+        {
+            GridInformation info = tilemap.GetComponent<GridInformation>();
+            if (info == null)
+                info = tilemap.GetComponent<Transform>().gameObject.AddComponent<GridInformation>();
+            return info;
+        }
+
+        void SetTileTag(Tilemap tilemap, Vector3Int position)
+        {
+            GetOrCreateInfo(tilemap).SetPositionProperty(position, GrassTileTag.Name, GrassTileTag.Grass);
+        }
+
+        int GetTileTag(Tilemap tilemap, Vector3Int position)
+        {
+            return GetOrCreateInfo(tilemap).GetPositionProperty(position, GrassTileTag.Name, GrassTileTag.None);
+        }
+
+        void EraseTileTag(Tilemap tilemap, Vector3Int position)
+        {
+            GetOrCreateInfo(tilemap).ErasePositionProperty(position, GrassTileTag.Name);
+        }
+
+        private void UpdateAround(Tilemap tilemap, Vector3Int position)
+        {
+            tilemap.RefreshTile(position);
+
+            for (int yd = 0; yd <= 1; yd++)
+            {
+                for (int xd = -1; xd <= 0; xd++)
+                {
+                    Vector3Int posAround = new Vector3Int(position.x + xd, position.y + yd, position.z);
+                    if (GetTileTag(tilemap, posAround) == GrassTileTag.Grass)
+                        return;
+                }
+            }
+
+            tilemap.SetTile(position, null);
+        }
+        
+        public override void Pick(GridLayout grid, GameObject brushTarget, BoundsInt position, Vector3Int pickStart)
+        {
+            var tilemap = brushTarget.GetComponent<Tilemap>();
+            if (tilemap == null)
+                return;
+            
+            Tile = tilemap.GetTile(new Vector3Int(position.xMin, position.yMin, position.zMin)) as GrassTile;
+        }
+        
+        public override void Paint(GridLayout grid, GameObject brushTarget, Vector3Int position)
+        {
+            var tilemap = brushTarget.GetComponent<Tilemap>();
+            if (tilemap == null)
+                return;
+
+            SetTileTag(tilemap, position);
+
+            for (int yd = -1; yd <= 0; yd++)
+            {
+                for (int xd = 0; xd <= 1; xd++)
+                {
+                    Vector3Int posAround = new Vector3Int(position.x + xd, position.y + yd, position.z);
+                    TileBase tile = tilemap.GetTile(posAround);
+                    if (tile == null)
+                        tilemap.SetTile(posAround, Tile);
+
+                    tilemap.RefreshTile(posAround);
+                }
+            }
+        }
+        
+        public override void Erase(GridLayout grid, GameObject brushTarget, Vector3Int position)
+        {
+            var tilemap = brushTarget.GetComponent<Tilemap>();
+            if (tilemap == null)
+                return;
+
+            EraseTileTag(tilemap, position);
+
+            for (int yd = -1; yd <= 0; yd++)
+            {
+                for (int xd = 0; xd <= 1; xd++)
+                {
+                    UpdateAround(tilemap, new Vector3Int(position.x + xd, position.y + yd, position.z));
+                }
+            }
+        }
+        /*
+        public override void FloodFill(GridLayout grid, GameObject brushTarget, Vector3Int position)
+        {
+            //var zPosition = new Vector3Int(position.x, position.y, z);
+            //base.FloodFill(grid, brushTarget, zPosition);
+        }
+        
+        public override void BoxFill(GridLayout gridLayout, GameObject brushTarget, BoundsInt bounds)
+        {
+            var tilemap = brushTarget.GetComponent<Tilemap>();
+            if (tilemap == null)
+                return;
+            for (int yd = bounds.yMin; yd < bounds.yMax; yd++)
+            {
+                for (int xd = bounds.xMin; xd < bounds.xMax; xd++)
+                {
+                    DrawTile(tilemap, new Vector3Int(xd, yd, bounds.z));
+                }
+            }
+        }*/
+    }
+
+    [CustomEditor(typeof(GrassBrush))]
+    public class GrassBrushEditor : GridBrushEditor
+    {
+        private GrassBrush Brush { get { return target as GrassBrush; } }
+        //private Tilemap tilemap;
+
+        /*
+        private void paintPreview(Tilemap tilemap, Vector3Int position)
+        {
+            tilemap.SetEditorPreviewTile(position, Brush.Tile);
+        }
+        public override void PaintPreview(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
+        {
+            tilemap = brushTarget.GetComponent<Tilemap>();
+            if (tilemap == null)
+                return;
+
+            tilemap.ClearAllEditorPreviewTiles();
+            paintPreview(tilemap, position);
+        }
+
+        public override void BoxFillPreview(GridLayout gridLayout, GameObject brushTarget, BoundsInt position)
+        {
+            tilemap = brushTarget.GetComponent<Tilemap>();
+            if (tilemap == null)
+                return;
+
+            tilemap.ClearAllEditorPreviewTiles();
+            //tilemap.EditorPreviewBoxFill(position.position, edgeBrush.Tile, position.x, position.y, position.x + position.size.x - 1, position.y + position.size.y - 1);
+            for (int yd = position.yMin; yd < position.yMax; yd++)
+            {
+                for (int xd = position.xMin; xd < position.xMax; xd++)
+                {
+                    paintPreview(tilemap, new Vector3Int(xd, yd, position.z));
+                    //PaintPreview(gridLayout, brushTarget, new Vector3Int(xd, yd, position.z));
+                }
+            }
+            //tilemap.EditorPreviewBoxFill(position.position, edgeBrush.Tile, position.xMin, position.yMin, position.xMax - 1, position.yMax - 1);
+        }*/
+        /*
+        public override void OnToolActivated(GridBrushBase.Tool tool)
+        {
+            base.OnToolActivated(tool);
+
+            if (tilemap == null)
+                return;
+
+            tilemap.ClearAllEditorPreviewTiles();
+        }
+        
+        public override void OnToolDeactivated(GridBrushBase.Tool tool)
+        {
+            base.OnToolDeactivated(tool);
+
+            if (tilemap == null)
+                return;
+            
+            tilemap.ClearAllEditorPreviewTiles();
+        }
+        
+        */
+        public override void OnPaintSceneGUI(GridLayout grid, GameObject brushTarget, BoundsInt position, GridBrushBase.Tool tool, bool executing)
+        {
+            base.OnPaintSceneGUI(grid, brushTarget, position, tool, executing);
+
+            if (position.z != 0)
+            {
+                var zPosition = new Vector3Int(position.min.x, position.min.y, position.z);
+                BoundsInt newPosition = new BoundsInt(zPosition, position.size);
+                Vector3[] cellLocals = new Vector3[]
+                {
+                    grid.CellToLocal(new Vector3Int(newPosition.min.x, newPosition.min.y, newPosition.min.z)),
+                    grid.CellToLocal(new Vector3Int(newPosition.max.x, newPosition.min.y, newPosition.min.z)),
+                    grid.CellToLocal(new Vector3Int(newPosition.max.x, newPosition.max.y, newPosition.min.z)),
+                    grid.CellToLocal(new Vector3Int(newPosition.min.x, newPosition.max.y, newPosition.min.z))
+                };
+
+                Handles.color = Color.blue;
+                int i = 0;
+                for (int j = cellLocals.Length - 1; i < cellLocals.Length; j = i++)
+                {
+                    Handles.DrawLine(cellLocals[j], cellLocals[i]);
+                }
+            }
+
+            
+            var labelText = "Pos: " + new Vector2Int(position.x, position.y);
+            if (position.size.x > 1 || position.size.y > 1) {
+                labelText += " Size: " + new Vector2Int(position.size.x, position.size.y);
+            }
+
+            Handles.Label(grid.CellToWorld(new Vector3Int(position.x, position.y, position.z)), labelText);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUI.BeginChangeCheck();
+            Brush.Tile = (GrassTile)EditorGUILayout.ObjectField("GrassTile", Brush.Tile, typeof(GrassTile), false, null);
+            if (EditorGUI.EndChangeCheck())
+                EditorUtility.SetDirty(Brush);
+        }
+    }
+}
---
+++ Assets/Tilemap/Tiles/Grass Tile/Scripts/GrassTile.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+
+namespace UnityEngine.Tilemaps
+{
+    public class GrassTileTag
+    {
+        public const string Name = "GrassTileTag";
+        public const int None = 0;
+        public const int Grass = 1;
+    }
+
+    [Serializable]
+	[CreateAssetMenu(fileName = "New Grass Tile", menuName = "Tiles/Grass Tile")]
+	public class GrassTile : TileBase
+	{
+        [SerializeField]
+		public Sprite[] m_Sprites;
+
+        GridInformation GetOrCreateInfo(ITilemap tilemap)
+        {
+            GridInformation info = tilemap.GetComponent<GridInformation>();
+            if (info == null)
+                info = tilemap.GetComponent<Transform>().gameObject.AddComponent<GridInformation>();
+            return info;
+        }
+
+        int GetTileTag(ITilemap tilemap, Vector3Int position)
+        {
+            return GetOrCreateInfo(tilemap).GetPositionProperty(position, GrassTileTag.Name, GrassTileTag.None);
+        }
+
+        public override void GetTileData(Vector3Int location, ITilemap tileMap, ref TileData tileData)
+        {
+            tileData.transform = Matrix4x4.identity;
+            tileData.color = Color.white;
+
+            int index = TileValue(tileMap, location + new Vector3Int(-1, 1, 0)) ? 1 : 0;
+            index += TileValue(tileMap, location + new Vector3Int(0, 1, 0)) ? 2 : 0;
+            index += TileValue(tileMap, location + new Vector3Int(0, 0, 0)) ? 4 : 0;
+            index += TileValue(tileMap, location + new Vector3Int(-1, 0, 0)) ? 8 : 0;
+            
+            if (index >= 0 && index < m_Sprites.Length)
+            {
+                tileData.sprite = m_Sprites[index];
+                tileData.flags = TileFlags.LockTransform | TileFlags.LockColor;
+                tileData.colliderType = Tile.ColliderType.Sprite;
+            }
+        }
+
+		private bool TileValue(ITilemap tileMap, Vector3Int position)
+        {
+            return GetTileTag(tileMap, position) == GrassTileTag.Grass;
+        }
+    }
+
+#if UNITY_EDITOR
+	[CustomEditor(typeof(GrassTile))]
+	public class GrassTileEditor : Editor
+	{
+		private GrassTile tile { get { return (target as GrassTile); } }
+
+		public void OnEnable()
+		{
+			if (tile.m_Sprites == null || tile.m_Sprites.Length != 16)
+			{
+				tile.m_Sprites = new Sprite[16];
+				EditorUtility.SetDirty(tile);
+			}
+		}
+
+		public override void OnInspectorGUI()
+		{
+			EditorGUILayout.LabelField("Place sprites shown based on the contents of the sprite.");
+			EditorGUILayout.Space();
+
+			float oldLabelWidth = EditorGUIUtility.labelWidth;
+			EditorGUIUtility.labelWidth = 210;
+
+			EditorGUI.BeginChangeCheck();
+            tile.m_Sprites[0] = (Sprite)EditorGUILayout.ObjectField("0", tile.m_Sprites[0], typeof(Sprite), false, null);
+            tile.m_Sprites[1] = (Sprite)EditorGUILayout.ObjectField("1", tile.m_Sprites[1], typeof(Sprite), false, null);
+            tile.m_Sprites[2] = (Sprite)EditorGUILayout.ObjectField("2", tile.m_Sprites[2], typeof(Sprite), false, null);
+            tile.m_Sprites[3] = (Sprite)EditorGUILayout.ObjectField("3", tile.m_Sprites[3], typeof(Sprite), false, null);
+            tile.m_Sprites[4] = (Sprite)EditorGUILayout.ObjectField("4", tile.m_Sprites[4], typeof(Sprite), false, null);
+            tile.m_Sprites[5] = (Sprite)EditorGUILayout.ObjectField("5", tile.m_Sprites[5], typeof(Sprite), false, null);
+            tile.m_Sprites[6] = (Sprite)EditorGUILayout.ObjectField("6", tile.m_Sprites[6], typeof(Sprite), false, null);
+            tile.m_Sprites[7] = (Sprite)EditorGUILayout.ObjectField("7", tile.m_Sprites[7], typeof(Sprite), false, null);
+            tile.m_Sprites[8] = (Sprite)EditorGUILayout.ObjectField("8", tile.m_Sprites[8], typeof(Sprite), false, null);
+            tile.m_Sprites[9] = (Sprite)EditorGUILayout.ObjectField("9", tile.m_Sprites[9], typeof(Sprite), false, null);
+            tile.m_Sprites[10] = (Sprite)EditorGUILayout.ObjectField("10", tile.m_Sprites[10], typeof(Sprite), false, null);
+            tile.m_Sprites[11] = (Sprite)EditorGUILayout.ObjectField("11", tile.m_Sprites[11], typeof(Sprite), false, null);
+            tile.m_Sprites[12] = (Sprite)EditorGUILayout.ObjectField("12", tile.m_Sprites[12], typeof(Sprite), false, null);
+            tile.m_Sprites[13] = (Sprite)EditorGUILayout.ObjectField("13", tile.m_Sprites[13], typeof(Sprite), false, null);
+            tile.m_Sprites[14] = (Sprite)EditorGUILayout.ObjectField("14", tile.m_Sprites[14], typeof(Sprite), false, null);
+            tile.m_Sprites[15] = (Sprite)EditorGUILayout.ObjectField("15", tile.m_Sprites[15], typeof(Sprite), false, null);
+            if (EditorGUI.EndChangeCheck())
+				EditorUtility.SetDirty(tile);
+
+			EditorGUIUtility.labelWidth = oldLabelWidth;
+		}
+	}
+#endif
+}
